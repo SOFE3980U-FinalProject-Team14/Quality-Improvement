b2323d270c44ae79381111ed4133e1d395ae0493
CheezyMac <c.macleod@hotmail.ca>
2018-03-29 22:32:23 -0400

    * Fixed issue where equals() method did not check for null or invalid types

--- org/gjt/sp/jedit/syntax/Chunk.java
+++ org/gjt/sp/jedit/syntax/Chunk.java
@@ -815,10 +815,15 @@ public class Chunk extends Token
 			// should be called only from GlyphCache to
 			// compare with other keys, then explicit type
 			// checking and null checking are not necessary.
-			GlyphKey other = (GlyphKey)otherObject;
-			return token.equals(other.token)
-				&& font.equals(other.font)
-				&& context.equals(other.context);
+
+			if(otherObject instanceof GlyphKey)
+			{
+				GlyphKey other = (GlyphKey)otherObject;
+				return token.equals(other.token)
+					&& font.equals(other.font)
+					&& context.equals(other.context);
+			}
+			return false;
 		}
 
 		@Override
